3a021fdd3df72ef96b290cdb3035131e1ffa18f6
Adam Dupuis <adamdupuis@laposte.net>
2021-12-17 15:05:37 +0100

    add col-50

--- www/js/script.js
+++ www/js/script.js
@@ -11,28 +11,14 @@ function get_genres(genresIdList){
       for(let index = 0; index < res.data.length; index++){
         var found = genresIdList.find(function(element){
           if (element == res.data[index].id){
-            elementFound++;
-            console.log(elementFound)
-
-            if(elementFound % 2 != 0){
-              $('genre__selection').append(`
-                <div class ="row padding">
-                `)
-            }
 
             $('.genre__selection').append(`
               <div class="col-50">
                 <a href="/play/${res.data[index].id}">
                   <img src="${res.data[index].picture_medium}">
                 </a>
-                <div class="Row text-align-center answer">${res.data[index].name}</div>
+                <div class="Row text-align-center">${res.data[index].name}</div>
               </div>`)
-
-              if(elementFound % 2 == 0){
-                $('genre__selection').append(`
-                  </div>
-                  `)
-              }
           }
         })
       }
