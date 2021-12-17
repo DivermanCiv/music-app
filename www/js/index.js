dc967b4baf17077b68dad367aba1806fc1918977
LisTrik49 <clem.moka49@gmail.com>
2021-12-17 11:38:46 +0100

    add functions

--- www/js/index.js
+++ www/js/index.js
@@ -1,32 +1,31 @@
-
-
 function get_4_artists_by_genreId(genreId) {
 
     app.request({
-      url: "https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/" + genreId + "/artists", //URL de L'api
-      method: "GET", // Méthode 
+      url: "https://www.proxyscan.io/api/proxy?format=json/https://api.deezer.com/genre/" + genreId + "/artists", //URL de L'api
+      method: "GET", // Méthode
       crossDomain: true,
-      dataType: "json", // Important, sinon vous allez récupérer un string et non un objet
+      dataType: "jsonp", // Important, sinon vous allez récupérer un string et non un objet
       beforeSend: function () {
         // Avant de récupérer mes datas, j'affiche un loader 
         //(important quand on fait un traitement pour montrer qu'il est en cours +  empêcher les impatients de cliquer partout pendant le process !)
         app.dialog.preloader();
       },
-      success: function (res) {
-        let tab = []
+      success: async function (res) {
+
         for (let index = 0; index < 4; index++) {
+            
+            console.log(res[index]);
+            let tracklist = await get_tracklist_by_artist(res[index].id)
 
-            tracklist = get_tracklist_by_artist(res.data[index].id)
-
-            tab.push(get_tracklist_by_artist(res.data[index].id))
+            $('.answer-container').append(`
+            <div class="Row margin-60 padding">
+                <div class="Col text-align-center answer">${tracklist.title}</div>
+            </div>
+            `)
         
         }
         app.dialog.close();
-        $('.answer-container').append(`
-            <div class="Row margin-60 padding">
-                <div class="Col text-align-center answer">${res.data[index].name}</div>
-            </div>
-        `)
+
       },
       
       error: function(res){
@@ -36,27 +35,23 @@ function get_4_artists_by_genreId(genreId) {
 };
 
 
-function get_tracklist_by_artist(artistId){
+async function get_tracklist_by_artist(artistId){
 
-    app.request({
-        url: "https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/" + artistId + "/top", //URL de L'api
+    let result = await app.request({
+        url: "https://www.proxyscan.io/api/proxy?format=json/https://api.deezer.com/artist/" + artistId + "/top", //URL de L'api
         method: "GET", // Méthode 
         crossDomain: true,
-        dataType: "json", // Important, sinon vous allez récupérer un string et non un objet
-        beforeSend: function () {
-          // Avant de récupérer mes datas, j'affiche un loader 
-          //(important quand on fait un traitement pour montrer qu'il est en cours +  empêcher les impatients de cliquer partout pendant le process !)
-          app.dialog.preloader();
-        },
+        dataType: "jsonp", // Important, sinon vous allez récupérer un string et non un objet
         success: function (res) {
-            let tab = []
-            for (let index = 0; index < 4; index++) {
-                
-            }
-            app.dialog.close();
+            
+            let index = Math.floor(Math.random() * res.length);
+
+            return res[index];
+
         },
         error: function(res){
           console.log(res)
         }
-      })
+    })
+    return result;
 }
